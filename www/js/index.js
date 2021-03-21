d890d5651ff264b7bbd2755f77810dade78f87c5
Pariksha Roka <parikshanepali@gmail.com>
2021-03-21 12:22:26 +1100

    Binded the event with jquery

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,11 @@
 document.addEventListener("deviceready", deviceReadyTest, false);
 
 function deviceReadyTest() {
-  // binding the event
-  document.getElementById("user_signup").addEventListener("click", userSignup);
+  // binding the event with vanilla javascript
+  //   document.getElementById("user_signup").addEventListener("click", userSignup);
+
+  // binding the event with jquery
+  $("#user_signup").click(userSignup);
 }
 
 function userSignup() {
